0761f6400416c65670e52b33bbd719013477acaf
Sora <wonge299@newschool.edu>
2022-02-17 05:41:24 -0500

    fix

--- Core_int_Lab/Assignment_2/typeface_specimen.js
+++ Core_int_Lab/Assignment_2/typeface_specimen.js
@@ -1,1 +1,11 @@
 /* For the Slider */
+slider.oninput = () => {
+    //Change Span Value
+    const slider = document.getElementById('slider');
+    const pixels = document.getElementById('pixels');
+    pixels.innerHTML = slider.value;
+    
+
+    // Change the --font-size
+    document.querySelector('html').style.setProperty('--font-size', `${slider.value}px`)
+}
