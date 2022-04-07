b5426d3bd0aacf6a7edfb6c4e8bd91194af667e9
Sora <wonge299@newschool.edu>
2022-04-06 20:07:03 -0400

    fix

---
+++ Core_int_studio/Workshop/The_Archive/Project_3_pt6/draft5.js
@@ -0,0 +1,127 @@
+// Variables
+
+const airtableApiKey = "keyV7LgbJKWVZQMgS"
+
+const airtableDatabaseUrl1="https://api.airtable.com/v0/appr4ZwtYBwT8zgta/group1"
+const airtableDatabaseUrl2="https://api.airtable.com/v0/appr4ZwtYBwT8zgta/group2"
+const airtableDatabaseUrl3="https://api.airtable.com/v0/appr4ZwtYBwT8zgta/group3"
+
+
+const authenticatedUrl1 = airtableDatabaseUrl1 + "?api_key=" + airtableApiKey
+const authenticatedUrl2 = airtableDatabaseUrl2 + "?api_key=" + airtableApiKey
+const authenticatedUrl3 = airtableDatabaseUrl3 + "?api_key=" + airtableApiKey
+
+
+
+// References
+const group1ContainerElement = document.querySelector('#group1')
+const group2ContainerElement = document.querySelector('#group2')
+const group3ContainerElement = document.querySelector('#group3')
+
+
+
+
+// Application
+
+const fetchPromise1 = fetch(authenticatedUrl1)
+const jsonPromise1 = fetchPromise1.then((response) => {
+    return response.json()
+})
+jsonPromise1.then((data) => {
+    const records = data.records
+    for (let index = 0; index < records.length; index++) {
+        const name = records[index].fields.Name
+        const imageUrl = records[index].fields.Attachments[0].url
+        // Create Container element
+        const containerElement = document.createElement('div')
+        containerElement.classList.add('container')
+        // Create Image element
+        const imageElement = document.createElement('img')
+        imageElement.classList.add('image')
+        imageElement.setAttribute('src', imageUrl)
+        // Create  name Element
+        const  nameElement = document.createElement('p')
+         nameElement.classList.add('name')
+         nameElement.innerHTML =  name
+        // Add Container Element to DOM
+        group1ContainerElement.appendChild(containerElement)
+        // Add Image elements to container elements
+        containerElement.appendChild(imageElement)
+        // Add  name elements
+        containerElement.appendChild(nameElement)
+
+
+    }
+})
+
+const fetchPromise2 = fetch(authenticatedUrl2)
+const jsonPromise2 = fetchPromise2.then((response) => {
+    return response.json()
+})
+jsonPromise2.then((data) => {
+    const records = data.records
+    for (let index = 0; index < records.length; index++) {
+        const name = records[index].fields.Name
+        const imageUrl = records[index].fields.Attachments[0].url
+        // Create Container element
+        const containerElement = document.createElement('div')
+        containerElement.classList.add('container')
+        // Create Image element
+        const imageElement = document.createElement('img')
+        imageElement.classList.add('image')
+        imageElement.setAttribute('src', imageUrl)
+        // Create  name Element
+        const  nameElement = document.createElement('p')
+         nameElement.classList.add('name')
+         nameElement.innerHTML =  name
+        // Add Container Element to DOM
+        group2ContainerElement.appendChild(containerElement)
+        // Add Image elements to container elements
+        containerElement.appendChild(imageElement)
+        // Add  name elements
+        containerElement.appendChild(nameElement)
+
+
+    }
+})
+
+const fetchPromise3 = fetch(authenticatedUrl3)
+const jsonPromise3 = fetchPromise3.then((response) => {
+    return response.json()
+})
+jsonPromise3.then((data) => {
+    const records = data.records
+    for (let index = 0; index < records.length; index++) {
+        const name = records[index].fields.Name
+        const imageUrl = records[index].fields.Attachments[0].url
+        // Create Container element
+        const containerElement = document.createElement('div')
+        containerElement.classList.add('container')
+        // Create Image element
+        const imageElement = document.createElement('img')
+        imageElement.classList.add('image')
+        imageElement.setAttribute('src', imageUrl)
+        // Create  name Element
+        const  nameElement = document.createElement('p')
+         nameElement.classList.add('name')
+         nameElement.innerHTML =  name
+        // Add Container Element to DOM
+        group3ContainerElement.appendChild(containerElement)
+        // Add Image elements to container elements
+        containerElement.appendChild(imageElement)
+        // Add  name elements
+        containerElement.appendChild(nameElement)
+
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
