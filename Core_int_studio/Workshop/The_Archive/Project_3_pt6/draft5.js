cc6be180e8f5514dde1e252ed69899b776e7374c
Sora <wonge299@newschool.edu>
2022-04-07 09:34:17 -0400

    fix

--- Core_int_studio/Workshop/The_Archive/Project_3_pt6/draft5.js
+++ Core_int_studio/Workshop/The_Archive/Project_3_pt6/draft5.js
@@ -57,37 +57,6 @@ jsonPromise1.then((data) => {
     }
 })
 
-const fetchPromise2 = fetch(authenticatedUrl2)
-const jsonPromise2 = fetchPromise2.then((response) => {
-    return response.json()
-})
-jsonPromise2.then((data) => {
-    const records = data.records
-    for (let index = 0; index < records.length; index++) {
-        const name = records[index].fields.Name
-        const imageUrl = records[index].fields.Attachments[0].url
-        // Create Container element
-        const containerElement = document.createElement('div')
-        containerElement.classList.add('container')
-        // Create Image element
-        const imageElement = document.createElement('img')
-        imageElement.classList.add('image')
-        imageElement.setAttribute('src', imageUrl)
-        // Create  name Element
-        const  nameElement = document.createElement('p')
-         nameElement.classList.add('name')
-         nameElement.innerHTML =  name
-        // Add Container Element to DOM
-        group2ContainerElement.appendChild(containerElement)
-        // Add Image elements to container elements
-        containerElement.appendChild(imageElement)
-        // Add  name elements
-        containerElement.appendChild(nameElement)
-
-
-    }
-})
-
 const fetchPromise3 = fetch(authenticatedUrl3)
 const jsonPromise3 = fetchPromise3.then((response) => {
     return response.json()
