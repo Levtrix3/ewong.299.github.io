06b37ef9457cf26cd6a01d168cb639d269ed2671
Sora <wonge299@newschool.edu>
2022-04-28 01:30:38 -0400

    fix

---
+++ Core_int_studio/Workshop/Slot_Machine/Project_4_pt2/draft.js
@@ -0,0 +1,40 @@
+function getRandomNumber() {
+    return Math.floor(Math.random() * 2) + 1;
+}
+
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+function spin() {
+    const item1 = getElement('item1');
+    const item2 = getElement('item2');
+    const item3 = getElement('item3');
+
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    const num3 = getRandomNumber();
+
+    item1.innnerHTML = `${num1}`;
+    item2.innnerHTML = `${num2}`;
+    item3.innnerHTML = `${num3}`;
+
+    if (num1 == num2 && num1 == num3) {
+        showMessage();
+    } else {
+        hideMessage();
+    }
+
+    function showMessage() {
+        const msg = document.getElementById('message');
+        msg.style.display = "block";
+        msg.classList.add('animated','pulse')
+    }
+
+    function hideMessage() {
+        const msg = document.getElementById('message');
+        msg.style.display = "none";
+    }
+
+
+}
